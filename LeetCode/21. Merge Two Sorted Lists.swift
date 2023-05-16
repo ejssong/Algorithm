d24d407a55dd4712cbac05042ac464023611a1cf
ejssong <ejsong0101@gmail.com>
2023-05-16 14:10:31 +0900

    :rocket: Daily Algorithm

---
+++ LeetCode/21. Merge Two Sorted Lists.swift
@@ -0,0 +1,35 @@
+//
+//  21. Merge Two Sorted Lists.swift
+//  
+//
+//  Created by ejsong on 2023/05/16.
+//
+
+import Foundation
+
+// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil { return list2 }
+        if list2 == nil { return list1 }
+        
+        var result: ListNode?
+        
+        if list1!.val <= list2!.val {
+            result = list1
+            result?.next = mergeTwoLists(list1?.next , list2)
+        }else {
+            result = list2
+            result?.next = mergeTwoLists(list1 , list2?.next)
+        }
+        return result
+    }
+}
